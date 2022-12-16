3facc54e0710fd779ed43e382f1d989e3fe836dd
Shadab Akhtar <akhtarshadab415@gmail.com>
2022-12-16 16:21:03 +0530

    minor footer ui changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
-import { Typography, Divider, Box } from '@mui/material';
+import { Typography, Divider, Box, Container } from '@mui/material';
 import React from 'react';
+import yodaplus from 'assets/images/yodaplus.png';
 
 const Footer = () => {
   return (
@@ -9,17 +10,49 @@ const Footer = () => {
           mt: 4
         }}
       />
-      <Typography
-        variant="body1"
-        align="center"
-        sx={{
-          py: 2,
-          // color: '#161c2d'
-          color: '#fff'
-        }}
-      >
-        &copy; {new Date().getFullYear()}, Powered By XDC Network
-      </Typography>
+      <Container>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <Typography
+            variant="body1"
+            // align="center"
+            sx={{
+              py: 2,
+              // color: '#161c2d'
+              color: '#fff'
+            }}
+          >
+            &copy; {new Date().getFullYear()}, Powered By XDC Network
+          </Typography>
+          <Typography
+            variant="body1"
+            // align="center"
+            onClick={() => window.open('https://yodaplus.com', '_blank')}
+            sx={{
+              py: 2,
+              // color: '#161c2d'
+              color: '#fff',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              cursor: 'pointer'
+            }}
+          >
+            Developed By {'\u00A0'}
+            <span>
+              <Box component="img" src={yodaplus} sx={{ height: '1.8rem' }} alt="Comtech Gold" />
+            </span>
+            {'\u00A0'} Yodaplus
+            {/* <span style={{ color: '#0D7350' }}>Yoda</span>
+            <span style={{ color: '#7E7E7D' }}>plus</span> */}
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 };
