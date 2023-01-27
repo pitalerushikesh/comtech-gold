74e53ce9a1803998784e7db2223e4a1668cc41ed
Shadab Akhtar <akhtarshadab415@gmail.com>
2023-01-27 18:24:54 +0530

    mainnet deployment Try 6

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 docker context use default
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 135135176603.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 135135176603.dkr.ecr.ap-south-1.amazonaws.com
 
 docker-compose \
   -f infra/docker-compose.base.yml \
   -f infra/docker-compose.build.yml \
-  -f infra/docker-compose.tags.yml \
-  --project-name comtech-apothem \
+  -f infra/docker-compose.mainnet-tags.yml \
+  --project-name comtech-gold-mainnet \
   --project-directory ./ \
   build
 
-docker push 135135176603.dkr.ecr.us-east-1.amazonaws.com/comtech-frontend:apothem
-docker push 135135176603.dkr.ecr.us-east-1.amazonaws.com/comtech-backend:apothem
-docker push 135135176603.dkr.ecr.us-east-1.amazonaws.com/comtech-redis:apothem
+docker push 135135176603.dkr.ecr.ap-south-1.amazonaws.com/comtech-frontend:mainnet
+docker push 135135176603.dkr.ecr.ap-south-1.amazonaws.com/comtech-backend:mainnet
+docker push 135135176603.dkr.ecr.ap-south-1.amazonaws.com/comtech-redis:mainnet
 
-docker context use yodaplus-comtech
+docker context use yodaplus
 
 cp .env ./infra/.env
 
 docker compose \
   -f infra/docker-compose.base.yml \
-  -f infra/docker-compose.tags.yml \
+  -f infra/docker-compose.mainnet-tags.yml \
   -f infra/docker-compose.ecs.yml \
-  --project-name comtech-apothem \
+  -f infra/docker-compose.ecs-mainnet.yml \
+  --project-name comtech-gold-mainnet \
   --project-directory ./ \
   up
 
